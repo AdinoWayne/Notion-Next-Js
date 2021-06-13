d00dff0aae278ab30397425d0f1eccb4a46b7cbd
Gnurt10 <gnurt10@gmail.com>
2021-06-13 23:14:24 +0700

    [update] add format notion api

--- src/lib/notion/rpc.ts
+++ src/lib/notion/rpc.ts
@@ -1,6 +1,21 @@
 import fetch, { Response } from 'node-fetch'
 import { API_ENDPOINT, NOTION_TOKEN } from './server-constants'
 
+export function getFullBlockId(blockId: string) {
+  if (typeof blockId !== 'string') {
+      throw Error(`blockId: ${typeof blockId} must be string`)
+  }
+  if (blockId.match("^[a-zA-Z0-9]+$")) {
+      return blockId.substr(0, 8) + "-"
+          + blockId.substr(8, 4) + "-"
+          + blockId.substr(12, 4) + "-"
+          + blockId.substr(16, 4) + "-"
+          + blockId.substr(20, 32)
+  } else {
+      return blockId
+  }
+}
+
 export default async function rpc(fnName: string, body: any) {
   if (!NOTION_TOKEN) {
     throw new Error('NOTION_TOKEN is not set in env')
